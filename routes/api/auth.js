63285d82448150879c5f3e6d0faa146c474f0a59
a daana <notaliald1@gmail.com>
2021-11-30 14:18:18 +0200

    get authenticated user by token

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,12 +1,21 @@
 const express = require("express");
 const router = express.Router();
+const auth = require("../../middleware/auth");
+const User = require("../../models/User");
 
 // @route   GET api/auth
 // @desc    Test route
 // @access  Public (don't need to be authenticated with Token )
 
-router.get("/", (req, res) => {
-  res.send("Auth route");
+router.get("/", auth, async (req, res) => {
+  // return user data
+  try {
+    const user = await User.findById(req.user.id).select("-password");
+    res.json(user);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Server error");
+  }
 });
 
 module.exports = router;
