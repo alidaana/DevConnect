b762a8b0f56d9caf0b23d3795f25fe43f3936eaa
a daana <notaliald1@gmail.com>
2021-11-30 19:17:11 +0200

    created Posts Schema

---
+++ models/Posts.js
@@ -0,0 +1,60 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const PostSchema = new Schema({
+  user: {
+    type: Schema.Types.ObjectId,
+    ref: "users",
+  },
+  text: {
+    type: String,
+    required: true,
+  },
+  // Name of the user not the post
+  name: {
+    type: String,
+  },
+  avatar: {
+    type: String,
+  },
+  // Array of likes
+  likes: [
+    {
+      // To know which like came from wich user
+      user: {
+        type: Schema.Types.ObjectId,
+        ref: "users",
+      },
+    },
+  ],
+  // Array of comments for the post
+  comments: [
+    {
+      // To know which comment came from wich user
+      user: {
+        type: Schema.Types.ObjectId,
+        ref: "users",
+      },
+      text: {
+        type: String,
+        required = true
+      },
+      name: {
+        type: String,
+      },
+      avatar: {
+        type: String,
+      },
+      date:{
+          type:Date,
+          default: Date.now
+      }
+    },
+  ],
+  date:{
+      type:Date,
+      default: Date.now
+  }
+});
+
+module.exports = Post = mongoose.model('post',PostSchema);
