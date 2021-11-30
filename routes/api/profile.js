fab37c485dbf0647350f0cb07850556a688f0106
a daana <notaliald1@gmail.com>
2021-11-30 17:04:55 +0200

    get user github account

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -5,6 +5,8 @@ const Profile = require("../../models/Profile");
 const User = require("../../models/User");
 const { check, validationResult } = require("express-validator");
 const { compare } = require("bcryptjs");
+const request = require("request");
+const config = require("config");
 
 // @route   GET api/profile/me
 // @desc    Get current user profile
@@ -233,7 +235,7 @@ router.put(
     [
       check("school", "school is required").not().isEmpty(),
       check("degree", "degree is required").not().isEmpty(),
-      check("Fieldofstudy", "Fieldofstudy is required").not().isEmpty(),
+      check("fieldofstudy", "Fieldofstudy is required").not().isEmpty(),
       check("from", "from is required").not().isEmpty(),
     ],
   ],
@@ -284,6 +286,36 @@ router.delete("/education/:edu_id", auth, async (req, res) => {
     await profile.save();
 
     res.json(profile);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Server error");
+  }
+});
+
+// @route   GET api/profile/github/:username
+// @desc    get user repos from github
+// @access  public
+router.get("/github/:username", (req, res) => {
+  try {
+    const options = {
+      uri: `https://api.github.com/users/${
+        req.params.username
+      }/repos?per_page=5&sort=created:asc&client_id=${config.get(
+        "githubClientId"
+      )}&client_secret=${config.get("githubSecret")}`,
+      method: "GET",
+      headers: { "user-agent": "node.js" },
+    };
+
+    request(options, (error, response, body) => {
+      if (error) console.error(error);
+
+      if (response.statusCode !== 200) {
+        return res.status(404).json({ msg: "No Github found" });
+      }
+
+      res.json(JSON.parse(body));
+    });
   } catch (err) {
     console.error(err.message);
     res.status(500).send("Server error");
