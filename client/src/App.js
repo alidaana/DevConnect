8cc342a02edb756319127a5b899517d0657038f4
a daana <notaliald1@gmail.com>
2021-11-30 23:26:20 +0200

    React routers signup/register

--- client/src/App.js
+++ client/src/App.js
@@ -2,12 +2,25 @@ import "./App.css";
 import React, { Fragment } from "react";
 import { Navbar } from "./components/layout/Navbar";
 import { Landing } from "./components/layout/Landing";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Login } from "./components/layout/auth/Login";
+import { Register } from "./components/layout/auth/Register";
 
 const App = () => (
-  <Fragment className="App">
-    <Navbar />
-    <Landing />
-  </Fragment>
+  <Router>
+    <Fragment>
+      <Navbar />
+      <Routes>
+        <Route exact path="/" element={<Landing />} />
+      </Routes>
+      <section className="container">
+        <Routes>
+          <Route exact path="/register" element={<Register />} />
+          <Route exact path="/login" element={<Login />} />
+        </Routes>
+      </section>
+    </Fragment>
+  </Router>
 );
 
 export default App;
