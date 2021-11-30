8f2e2736ca980552edc8205773a487c7ab611af8
a daana <notaliald1@gmail.com>
2021-11-30 13:57:28 +0200

    setup jwt on register

--- routes/api/users.js
+++ routes/api/users.js
@@ -3,6 +3,8 @@ const router = express.Router();
 const { check, validationResult } = require("express-validator");
 const gravatar = require("gravatar");
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const config = require("config");
 
 const User = require("../../models/User");
 // @route   POST api/users
@@ -59,13 +61,28 @@ router.post(
       await user.save();
 
       // 4. return jsonwebtoken to login after regestration is complete
-      res.send("User registered");
+      const payload = {
+        user: {
+          id: user.id,
+        },
+      };
+
+      jwt.sign(
+        payload,
+        config.get("jwtSecret"),
+        { expiresIn: 360000 },
+        (err, token) => {
+          if (err) {
+            throw err;
+          } else {
+            res.json({ token });
+          }
+        }
+      );
     } catch (err) {
       console.error(err.message);
       res.status(500).send("Server error");
     }
-
-    res.send("User route");
   }
 );
 
