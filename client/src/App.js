4b887eb440a8cc2c13303dc8d34ac80bc682dc95
a daana <notaliald1@gmail.com>
2021-12-01 19:14:12 +0200

    setup Redux

--- client/src/App.js
+++ client/src/App.js
@@ -5,22 +5,27 @@ import { Landing } from "./components/layout/Landing";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { Login } from "./components/layout/auth/Login";
 import { Register } from "./components/layout/auth/Register";
+// Redux
+import { Provider } from "react-redux";
+import store from "./store";
 
 const App = () => (
-  <Router>
-    <Fragment>
-      <Navbar />
-      <Routes>
-        <Route exact path="/" element={<Landing />} />
-      </Routes>
-      <section className="container">
+  <Provider store={store}>
+    <Router>
+      <Fragment>
+        <Navbar />
         <Routes>
-          <Route exact path="/register" element={<Register />} />
-          <Route exact path="/login" element={<Login />} />
+          <Route exact path="/" element={<Landing />} />
         </Routes>
-      </section>
-    </Fragment>
-  </Router>
+        <section className="container">
+          <Routes>
+            <Route exact path="/register" element={<Register />} />
+            <Route exact path="/login" element={<Login />} />
+          </Routes>
+        </section>
+      </Fragment>
+    </Router>
+  </Provider>
 );
 
 export default App;
